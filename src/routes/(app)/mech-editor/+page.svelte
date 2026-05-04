3cc0c25d21b026e806778a65edac4ebe9a7e845a
shotgun175 <bharatvalluru@gmail.com>
2026-05-04 13:19:31 -0400

    fix: mech-editor height 100% for flex layout, remove duplicate mech-pulse keyframe

--- src/routes/(app)/mech-editor/+page.svelte
+++ src/routes/(app)/mech-editor/+page.svelte
@@ -54,7 +54,7 @@
 
 <Header title="Raid Editor" />
 
-<div style="display: flex; overflow: hidden; height: calc(100vh - 64px);">
+<div style="display: flex; overflow: hidden; height: 100%;">
   <GateSidebar />
 
   {#if gate}
